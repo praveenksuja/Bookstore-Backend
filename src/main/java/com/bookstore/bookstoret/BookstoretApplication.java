73782e08941e8b450d503afdbf7b2c4b46f0c22b
Praveen Kumar <praveenkt62@gmail.com>
2017-09-04 14:01:14 -0400

    created bookstore backend

---
+++ src/main/java/com/bookstore/bookstoret/BookstoretApplication.java
@@ -0,0 +1,12 @@
+package com.bookstore.bookstoret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoretApplication.class, args);
+	}
+}
